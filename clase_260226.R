6308fdde599672615e59c920f1caefc67bf7aab3
AmyCV15 <chavezamairani@gmail.com>
2026-03-02 21:37:52 -0600

    Clase jueves 260226 y ejercicios tarea

---
+++ clase_260226.R
@@ -0,0 +1,230 @@
+###practica de Tidyverse ####
+#clase del 260225 #
+
+#Liberías necesarias para trabajar 
+# Cargamos lo que usaremos en todo el tutorial
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tidyr)
+url_tinto  <- "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+url_blanco <- "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
+
+vino_tinto  <- read_delim(url_tinto,  delim = ";", show_col_types = FALSE)
+vino_blanco <- read_delim(url_blanco, delim = ";", show_col_types = FALSE)
+
+# Añadimos columna "tipo" para identificarlos al unirlos
+vino_tinto  <- mutate(vino_tinto,  tipo = "tinto")
+vino_blanco <- mutate(vino_blanco, tipo = "blanco")
+
+# bind_rows() apila las dos tablas (como pegar una encima de la otra)
+vinos <- bind_rows(vino_tinto, vino_blanco)
+
+glimpse(vinos)
+
+#Ahora con otra base de datos diferente, desde un archivo local.
+
+salud <- read_csv("01_RawData/dataset_categorical_NA.csv", show_col_types = FALSE)
+glimpse(salud)
+
+#probando las ventajas de pipe
+vinos |>
+  filter(tipo == "tinto") |>
+  filter(quality >= 7) |>
+  select(tipo, quality, alcohol) |>
+  arrange(desc(alcohol)) |>
+  head(8)
+
+salud |>
+  filter(SmokingStatus == "Fuma") |>
+  select(ID, Age, BMI,
+         Cholesterol) |>
+  head(8)
+#varias formas de hacerlo 
+
+# DPLYR: mismo resultado, más claro
+filter(vinos, quality == 9)
+vinos[vinos$quality == 9, ]
+#tambien funciona así 
+vinos %>% filter(quality == 9)
+# Participantes con BMI mayor a 30 (obesidad)
+filter(salud, BMI > 30)
+
+salud %>% 
+  filter(BMI <= 99.0) %>% 
+  select(ID, Age, BMI,
+         Cholesterol) #esto no sale revisalo 
+#Hay que poner select para que pueda regresar un resultado
+salud %>% 
+  filter(BMI <= 99.0 & BMI >= 20) #ahora si sale. Algo pasa con números enteros
+
+#con vinos sí funciona
+vinos %>% 
+  filter(tipo == "tinto" & quality >= 8)
+
+
+# otro 
+filter(vinos, tipo == "tinto" & quality == 5)
+#
+# Participantes que fuman Y tienen BMI mayor a 25
+filter(salud, SmokingStatus == "Fuma", BMI > 25)
+
+#Datos condiciones con OR
+# Vinos que tengan quality == 9 O alcohol > 14
+filter(vinos, quality == 9 | alcohol > 14) |>
+  select(tipo, quality, alcohol) |>
+  head(8)
+
+#Filtrar por varios valores
+# Vinos de calidad 8 o 9
+filter(vinos, quality %in% c(8, 9)) |>
+  select(tipo, quality, alcohol) |>
+  head(8)
+#filtrar por rangos 
+# Vinos con pH entre 3.0 y 3.1 (incluyendo los extremos)
+filter(vinos, between(pH, 3.0, 3.1)) |>
+  select(tipo, pH, quality) |>
+  head(6)
+#Manejar NA en los filtros 
+# Participantes sin dato de edad O menores de 30
+filter(salud, is.na(Age) | Age < 30) |>
+  select(ID, Age, SmokingStatus)
+filter(salud, is.na(Age) ) |>
+  select(ID, Age, SmokingStatus)
+#para ver cuantos NA son por columnas seleccionadas 
+salud %>%  
+  filter(is.na(Age)) %>% 
+  count()
+
+
+#### Ejercicios con filter ###
+#1. ¿Cuántos vinos blancos tienen una calidad de 5 o menos?
+vinos %>% 
+  filter(tipo == "blanco") %>% 
+  filter(quality <= 5) %>% 
+  count()
+#otra forma más rápida
+vinos %>% 
+  filter(tipo == "blanco", quality <= 5) %>% 
+  count()
+
+#Filtra los vinos tintos que tengan alcohol mayor al promedio de todos los vinos.
+#Pista: puedes usar mean(vinos$alcohol) como valor de referencia dentro del filter.
+
+vinos %>%  
+  filter(tipo == "tinto", alcohol > mean(alcohol))
+#también funciona así 
+vinos %>%  
+  filter(alcohol > mean(vinos$alcohol))
+
+#3. ¿Cuántos participantes del dataset salud son ex-fumadores y residen en zona urbana?
+head(salud$SmokingStatus)
+head(salud$ResidenceType)
+salud %>% 
+  filter(SmokingStatus == "Ex-fumadora", ResidenceType == "Urbano") %>% 
+  count()
+
+#4. Filtra los participantes con nivel de estrés mayor a 80. ¿Qué estatus de 
+#tabaquismo predomina entre ellos?
+head(salud$StressLevel)
+salud %>% 
+  filter(StressLevel > 80) %>% 
+  count(SmokingStatus, sort = TRUE)
+##### Verbo Selected!
+# R BASE: seleccionar columnas por nombre (con comillas y c())
+vinos[, c("tipo", "quality", "alcohol")]
+# DPLYR: más limpio, sin comillas, sin c()
+select(vinos, tipo, quality, alcohol)
+# Vinos: tres columnas clave
+vinos |>
+  select(tipo, quality, alcohol) |>
+  head(5)
+# Salud: perfil básico de cada participante
+salud |>
+  select(ID, Age, BMI, SmokingStatus, ResidenceType) |>
+  head(5)
+# Quitamos las columnas de dióxido de azufre
+vinos |>
+  select(-`free sulfur dioxide`, -`total sulfur dioxide`) |>
+  names()
+# Quitamos las medidas corporales directas del dataset de salud
+salud |>
+  select(-Weight, -Height, -WaistCircumference, -HipCircumference) |>
+  names()
+vinos |>
+  select(
+    tipo,
+    calidad        = quality,
+    alcohol,
+    acidez_volatil = `volatile acidity`
+  ) |>
+  head(4)
+salud |>
+  select(
+    participante = ID,
+    edad         = Age,
+    imc          = BMI,
+    tabaquismo   = SmokingStatus,
+    residencia   = ResidenceType
+  ) |>
+  head(4)
+#esto no cambia la base de datos a menos que yo lo indique renombrando 
+#en otro objeto la base de datos 
+# Columnas cuyo nombre empieza con "total" (vinos)
+vinos |>
+  select(tipo, quality, starts_with("total")) |>
+  head(4)
+# Columnas que contienen "Circumference" (salud)
+salud |>
+  select(ID, contains("Circumference")) |>
+  head(4)
+# Solo columnas numéricas (salud)
+salud |>
+  select(where(is.numeric)) |>
+  head(3)
+## Ejercicios 
+#1. Del dataset vinos, selecciona solo las columnas de texto (tipo character).
+vinos %>% 
+  select(where(is.character)) 
+#2. Del dataset vinos, selecciona todas las columnas cuyo nombre contenga 
+#la palabra "acid".
+vinos %>% 
+  select(contains("acid"))
+#3. Del dataset salud, selecciona ID, Age y todas las columnas que contienen 
+#medidas de circunferencia. Renombra WaistCircumference como cintura.
+
+salud %>% 
+  select(
+    ID, 
+    Age, 
+    contains("Circumference"), 
+    Cintura = WaistCircumference
+  )
+# 4. Filtra solo los participantes del dataset salud que residan en zona 
+#rural y luego selecciona únicamente ID, Age, BMI y SmokingStatus.
+head(salud$ResidenceType)
+salud %>%  
+  filter(ResidenceType == "Rural") %>% 
+  select(ID, Age, BMI, SmokingStatus)
+
+##Verbo 3. Arrange
+#Ejercicios 
+#1. ¿Cuáles son los 10 vinos con mayor pH? Muestra también su tipo y calidad.
+vinos %>% 
+  select(tipo, quality, pH) %>% 
+  arrange(pH) %>% 
+  head(10)
+#2.¿Qué participante del dataset salud tiene el nivel de glucosa más alto? 
+#Muestra el top 5 con su ID, edad y estatus de tabaquismo.
+salud %>% 
+  select(ID, Age, SmokingStatus, Glucose) %>% 
+  arrange(desc(Glucose)) %>% 
+  head(5)
+#3. Ordena los vinos blancos primero por calidad descendente y luego por 
+#acidez volátil (volatile acidity) ascendente. Muestra las primeras 10 filas.
+head(vinos)
+vinos %>% 
+  filter(tipo == "blanco") %>% 
+  arrange(desc(quality), volatile acidity) %>%   ###esto no sale corrígelo 
+  head(10)
+usethis::use_github()
